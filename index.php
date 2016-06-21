4b4e8c2785efdae3ba4a9a323d1b92924085e235
root <root@localhost.localdomain>
2016-06-20 20:30:12 -0400

    index.php and parseinfo.php basics are working

--- index.php
+++ index.php
@@ -1,3 +1,35 @@
-<?php
-phpinfo();
-?>
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>Luma - New Hire</title>
+  <meta name="description" content="Luma - New Hire">
+  <meta name="author" content="Systems">
+
+  <!--<link rel="stylesheet" href="css/styles.css?v=1.0">-->
+
+  </head>
+
+
+
+<body>
+
+
+<form action="parseinfo.php" method="post">
+First Name: <input type="text" name="fname"><br>
+Last Name: <input type="text" name="lname"><br>
+Initials: <input type="text" name="initials"><br>
+<input type="submit">
+</form>
+
+
+
+
+
+
+
+
+</body>
+</html>
---
+++ parseinfo.php
@@ -0,0 +1,19 @@
+<?php
+
+# collect form input and load into varibles
+$fName = $_POST["fname"];
+$lName = $_POST["lname"];
+$initials = $_POST["initials"];
+
+
+
+
+#console out
+echo $fName;
+echo "<br />";
+echo $lName;
+echo "<br />";
+echo $initials;
+
+
+?>
